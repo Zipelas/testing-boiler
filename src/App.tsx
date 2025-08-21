530bec9791f1f67062037bf8ba52a682a12837a3
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-21 13:35:31 +0200

    ran npm install again install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import MonthComponent from './components/Month';
 
 function App() {
   return (
-    <div>
-      <h1>Vite + React</h1>
-
-      <CounterButton />
+    <>
       <div>
+        {/* <h1>Vite + React</h1>
+
+        <CounterButton /> */}
+      </div>
+      <div className='bg-pink-300 text-[#663399] text-6xl text-center p-4'>
         <MonthComponent date={new Date()} />
       </div>
-      <div>
+      <div className='bg-pink-300 text-[#663399] text-6xl text-center p-4'>
         <DayComponent date={new Date()} />
       </div>
-    </div>
+    </>
   );
 }
 
