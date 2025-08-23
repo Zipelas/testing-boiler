8e7a52ff47ad9e9fbba8afb0de81870308aa9273
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-23 12:31:59 +0200

    added exclude files in coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   test: {
-    environment: "jsdom",
-    setupFiles: ["./src/test-setup.ts"],
-    reporters: ["default"],
+    environment: 'jsdom',
+    setupFiles: ['./src/test-setup.ts'],
+    reporters: ['default'],
     coverage: {
-      provider: "istanbul",
+      provider: 'istanbul',
+      reporter: ['text', 'lcov'],
+      exclude: [
+        '**/*.config.*',
+        '**/.eslintrc.*',
+        'src/main.ts?(x)',
+        'src/utils.ts?(x)',
+      ],
     },
   },
 });
