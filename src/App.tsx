d70de83b10f06c532c979bfd61d4d8d3b9fa828f
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-24 15:34:28 +0200

    added components and fixed divs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import DateComponent from './components/DateComponent';
 import DatePicker from './components/DatePicker';
 import DayComponent from './components/Day';
 import MonthComponent from './components/Month';
@@ -28,24 +29,41 @@ function App() {
   }
 
   return (
-    <div className='max-w-md mx-auto p-4'>
+    <div className='min-h-screen bg-gray-100 p-4'>
       <h1 className='text-2xl font-bold text-center mb-4 text-[#663399]'>
         Vite + React
       </h1>
       <h2 className='text-2xl font-bold text-center mb-4 text-[#663399]'>
         🎬 Filmlista
       </h2>
-      <DatePicker
-        selectedDate={selectedDate}
-        onDateChange={handleDateChange}
-      />
-      <MovieList movies={displayedMovies} />
-      <ResetButton onReset={handleReset} />
-      <div className='bg-pink-300 text-[#663399] text-6xl text-center p-4'>
-        <MonthComponent date={new Date()} />
+      <div className='flex justify-center p-6 space-x-6'>
+        <div>
+          <DatePicker
+            selectedDate={selectedDate}
+            onDateChange={handleDateChange}
+          />
+        </div>
+        <div className='border-2 rounded-4xl border-[#663399] text-[#663399] font-bold inline-block p-4'>
+          <MovieList movies={displayedMovies} />
+        </div>
       </div>
-      <div className='bg-pink-300 text-[#663399] text-6xl text-center p-4'>
-        <DayComponent date={new Date()} />
+      <div className='flex w-full p-6'>
+        {/* vänster (datumboxen) */}
+        <div className='flex-1'>
+          <div className='border-2 rounded-4xl border-[#663399] inline-block px-6 py-2'>
+            <DayComponent date={new Date()} />
+            <DateComponent date={new Date()} />
+            <MonthComponent date={new Date()} />
+          </div>
+        </div>
+
+        {/* mitten (ResetButton centrerad i sin kolumn) */}
+        <div className='flex-1 flex justify-center items-center'>
+          <ResetButton onReset={handleReset} />
+        </div>
+
+        {/* höger (tom för nu, men kan fyllas med annat senare) */}
+        <div className='flex-1'></div>
       </div>
     </div>
   );
